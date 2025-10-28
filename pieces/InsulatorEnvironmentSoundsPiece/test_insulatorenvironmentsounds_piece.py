9ce02ca6b080ec2724cbe2f43c1f4c371ff5529f
marian <trnka@savba.sk>
2025-10-28 11:57:57 +0100

    start

--- pieces/InsulatorEnvironmentSoundsPiece/test_insulatorenvironmentsounds_piece.py
+++ pieces/InsulatorEnvironmentSoundsPiece/test_insulatorenvironmentsounds_piece.py
@@ -1,14 +1,13 @@
 from domino.testing import piece_dry_run
 
-def test_insulatorcontamination_piece():
+def test_insulatorenvironmentsounds_piece():
     input_data = dict(
-        #y=audio_path,
         y='http://speech.savba.sk/DiCris/echo_20250715_094400_7B-0.wav',
-        sr=192000,
+        sr=-1,
     )
     piece_output = piece_dry_run(
-        piece_name="InsulatorHealthPiece",
+        piece_name="InsulatorEnvironmentSoundsPiece",
         input_data=input_data
     )
     assert piece_output is not None
-    assert piece_output["contamination"] is not None
+    assert piece_output["top_class"] is not None

2a6af9825a2cdff4f8ce08a4aacdea9ec5dc26f1
marian <trnka@savba.sk>
2025-10-28 13:33:16 +0100

    start

--- pieces/InsulatorEnvironmentSoundsPiece/tst_insulatorenvironmentsounds_piece.py
+++ pieces/InsulatorEnvironmentSoundsPiece/tst_insulatorenvironmentsounds_piece.py
@@ -5,11 +5,11 @@ def test_insulatorenvironmentsounds_piece():
 		y='http://speech.savba.sk/DiCris/echo_20250715_094400_7B-0.wav',
 		sr=-1,
 	)
-	'''
+
 	piece_output = piece_dry_run(
 		piece_name="InsulatorEnvironmentSoundsPiece",
 		input_data=input_data
 	)
 	assert piece_output is not None
 	assert piece_output["top_class"] is not None
-	'''
+	

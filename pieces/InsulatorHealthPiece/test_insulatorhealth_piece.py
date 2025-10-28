7cb3999bae5dfc839cd70922fb08f7eda0fc30bb
marian <trnka@savba.sk>
2025-10-28 12:44:56 +0100

    start

--- pieces/InsulatorHealthPiece/test_insulatorhealth_piece.py
+++ pieces/InsulatorHealthPiece/test_insulatorhealth_piece.py
@@ -2,15 +2,13 @@ from domino.testing import piece_dry_run
 
 def test_insulatorcontamination_piece():
 	input_data = dict(
-		#y=audio_path,
 		y='http://speech.savba.sk/DiCris/echo_20250715_094400_7B-0.wav',
 		sr=192000,
 	)
-	'''
+
 	piece_output = piece_dry_run(
 		piece_name="InsulatorHealthPiece",
 		input_data=input_data
 	)
 	assert piece_output is not None
 	assert piece_output["contamination"] is not None
-	'''
